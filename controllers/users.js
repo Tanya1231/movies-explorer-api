d6982be5032e3144a87f6b307b21185222ef644e
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2023-01-07 13:36:01 +0500

    0.3

--- controllers/users.js
+++ controllers/users.js
@@ -89,7 +89,7 @@ const login = async (req, res, next) => {
     res.cookie('jwt', token, {
       maxAge: 3600000,
       httpOnly: true,
-      sameSite: false,
+      sameSite: 'none',
     });
     return res.status(200).send({ message: AUTORIZATION_TEXT, token });
   } catch (err) {
