fea454b8808f9127108c5b0c78a728991ff8834c
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2023-01-10 10:12:55 +0500

    Внесены поравки в корс

--- middlewares/corsHandler.js
+++ middlewares/corsHandler.js
@@ -3,6 +3,8 @@ const allowedCors = [
   'https://localhost:3000',
   'http://localhost:3001',
   'https://localhost:3001',
+  'https://movies5.nomoredomains.club',
+  'http://movies5.nomoredomains.club/',
 ];
 
 const DEFAULT_ALLOWED_METHODS = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
